a8a4229ca6cc227cb5f51cdd13731d530f3be4a9
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-11-08 12:11:24 +0530

    Update db.js
    database hosted

--- project-root/database/db.js
+++ project-root/database/db.js
@@ -1,7 +1,10 @@
 // database/db.js
 const mongoose = require('mongoose');
+// mongodb://0.0.0.0:27017/MetaGrowth
+//mongodb+srv://satyamrau9721:F5s1wFUB35apNzdC@admin.zzp9cix.mongodb.net/MetaGrowth?retryWrites=true&w=majority
 
-mongoose.connect('mongodb://0.0.0.0:27017/MetaGrowth', {
+mongoose.connect('mongodb+srv://satyamrau9721:F5s1wFUB35apNzdC@admin.zzp9cix.mongodb.net/MetaGrowth?retryWrites=true&w=majority'
+, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
